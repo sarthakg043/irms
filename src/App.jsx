bff313cc99058295bd134c9f91532f475592d3ab
Sarthak Gupta <sarthakg4953@gmail.com>
2024-08-01 14:26:36 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import Layout from './Layout'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { DepartmentDashboard, NewForm, Rejected, UserDashboard } from './pages'
 import './App.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />} >
+    <Route path="incidentreport" element={<UserDashboard />} />
+    <Route path="report" >
+      <Route path="form" element={<NewForm />} />
+      <Route path="rejected" element={<Rejected />} />
+      <Route path="department" element={<DepartmentDashboard />} />
+    </Route>
+    <Route path="*" element={<div>Error: 404</div>} />
+  </Route>
+))
+
 
 function App() {
 
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import DepartmentDashboard from "./DepartmentDashboard/DepartmentDashboard";
+import UserDashboard from "./UserDashboard/UserDashboard";
+import NewForm from "./NewForm/NewForm";
+import Rejected from "./RejectedForm/Rejected";
+
+export { DepartmentDashboard, UserDashboard, NewForm, Rejected };
