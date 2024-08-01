51f3068cd2d3884aaf85e759a3896e47590e0f50
arshedvp <arshedvp20@gmail.com>
2024-08-01 23:51:50 +0530

    added navbar and changed the color scheme

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,12 @@
-import React from 'react' 
 import { Outlet } from 'react-router-dom'
 import { Footer, Navbar } from './components'
 
 const Layout = () => {
   return (
-    <div>
+    <div className='bg-[#d7dcd7] h-screen'>
         <Navbar />
-        <Outlet />
+        <div className='h-[80vh]'>&nbsp;</div>
+        <br /><br />
         <Footer />
     </div>
   )
